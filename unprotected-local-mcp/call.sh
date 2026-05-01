3a4cbe81e17b6cc9deb2781953cc1d516d8f4e2e
Dan Moore <dan@fusionauth.io>
2026-04-30 21:07:32 -0600

    sample calling script

---
+++ unprotected-local-mcp/call.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+curl -X POST http://localhost:8000/mcp \
+  -H "Content-Type: application/json" \
+  -H "Accept: application/json, text/event-stream" \
+  -d '{
+    "jsonrpc": "2.0",
+    "id": 1,
+    "method": "tools/call",
+    "params": {
+      "name": "get_name",
+      "arguments": {}
+    }
+  }'
+
+
