c90a8c54be26905ba95eda45aac5c9951e6aac29
Dan Moore <dan@fusionauth.io>
2026-05-01 09:02:14 -0600

    daemon mode

--- protected-remote-mcp/start.sh
+++ protected-remote-mcp/start.sh
@@ -3,4 +3,4 @@
 MCP_SERVER_URL=http://localhost:8000 \
 FUSIONAUTH_URL=https://pre-feature-resource-indicator-rfc8707.dev.us-west-2.fusionauth.co \
 FUSIONAUTH_EXTERNAL_URL=https://pre-feature-resource-indicator-rfc8707.dev.us-west-2.fusionauth.co \
-docker compose up
+docker compose up -d 
