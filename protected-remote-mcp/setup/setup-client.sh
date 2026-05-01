fe5a8bcaaddd7301e2275a4b1fb4e21ec52ecac1
Dan Moore <dan@fusionauth.io>
2026-04-30 20:50:25 -0600

    helper script to set up client

---
+++ protected-remote-mcp/setup/setup-client.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+python setup_clients.py \
+  --fusionauth-url https://pre-feature-resource-indicator-rfc8707.dev.us-west-2.fusionauth.co \
+  --api-key TODO \
+  --mcp-server-url http://localhost:8000 \
+  --email-address vital-sable@example.com
