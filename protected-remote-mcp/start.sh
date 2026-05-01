ef1a4ea3fab2dbb6407c0ae419f3ee06702c0851
Dan Moore <dan@fusionauth.io>
2026-04-30 20:51:46 -0600

    make start executable

--- protected-remote-mcp/start.sh
+++ protected-remote-mcp/start.sh
@@ -1,3 +1,5 @@
+#!/bin/sh
+
 MCP_SERVER_URL=http://localhost:8000 \
 FUSIONAUTH_URL=https://pre-feature-resource-indicator-rfc8707.dev.us-west-2.fusionauth.co \
 FUSIONAUTH_EXTERNAL_URL=https://pre-feature-resource-indicator-rfc8707.dev.us-west-2.fusionauth.co \
